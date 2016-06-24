42045a1937c99ccec04d8c907eab9e77f46692f9
hectorvalle <hectorvalle@hectorvalles-mac.local>
2016-06-24 11:41:14 -0500

    Initial Commit

---
+++ lab04b/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lab04b
+//
+//  Created by hectorvalle on 6/24/16.
+//  Copyright © 2016 hectorvalle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
